e0b6f4ea6b4a0e5b221c64836225e80a6c18d404
angiedanielar <angie.ruiz@mail.escuelaing.edu.co>
2020-04-16 20:53:26 -0500

    correccion de astah y bd con el tipo bigint

--- ScriptsSQL/CrearyPoblar.sql
+++ ScriptsSQL/CrearyPoblar.sql
@@ -1,23 +1,19 @@
-CREATE TABLE IF NOT EXISTS Rol(
-tipo VARCHAR(12) PRIMARY KEY);
+CREATE TABLE IF NOT EXISTS Rol(tipo VARCHAR(14) PRIMARY KEY);
 
-CREATE TABLE IF NOT EXISTS Usuario(
-  id NUMBER(12) NOT NULL,
-  tid VARCHAR(2) NOT NULL,
-  nombre VARCHAR(50) NOT NULL,
-  telefono NUMBER(12) NOT NULL,
-  correo VARCHAR(50) NOT NULL,
-  clave VARCHAR(20) NOT NULL,
-  estado VARCHAR(15) NOT NULL,
-  rol VARCHAR(12) REFERENCES Rol(tipo),
-  PRIMARY KEY (correo),
-  UNIQUE KEY (id),
-  UNIQUE KEY (telefono));
+CREATE TABLE IF NOT EXISTS Usuario(id BIGINT NOT NULL, tid VARCHAR(2) NOT NULL, nombre VARCHAR(50) NOT NULL, telefono BIGINT NOT NULL, correo VARCHAR(50) PRIMARY KEY, clave VARCHAR(20) NOT NULL, estado VARCHAR(15) NOT NULL, rol VARCHAR(12) REFERENCES Rol(tipo));
+
+ALTER TABLE Usuario ADD CONSTRAINT id_unico UNIQUE(id);
+
+CREATE TABLE IF NOT EXISTS Iniciativa (nombrePropuesta VARCHAR(50) NOT NULL, id SERIAL PRIMARY KEY, descripcion varchar(150)  NOT NULL, fechaInicio date  NOT NULL, area varchar(100)  NOT NULL, usuario_id INT REFERENCES Usuario(id), estado VARCHAR(50) NOT NULL);
+
+ALTER TABLE Iniciativa ADD CONSTRAINT nombrePropuesta_unico UNIQUE(nombrePropuesta);
+
+CREATE TABLE IF NOT EXISTS PalabraClave (id INT PRIMARY KEY, palabraClave VARCHAR(20) NOT NULL);
 
 INSERT INTO Rol(tipo) VALUES ('Administrador');
 INSERT INTO Rol(tipo) VALUES ('Proponente');
-INSERT INTO Rol(tipo) VALUES ('Personal PMO');
+INSERT INTO Rol(tipo) VALUES ('PersonalPMO');
 INSERT INTO Rol(tipo) VALUES ('Publico');
 
-INSERT INTO Usuario (id, tid, nombre, telefono, correo, clave, estado, rol) VALUES (1234, 'CC', 'Angi Jimenez', 9303130, 'angi.jimenez', 'Angie2000', 'Activo', 'Administrador');
-INSERT INTO Usuario (id, tid, nombre, telefono, correo, clave, estado, rol) VALUES (12345, 'CC', 'Daniela Ruiz', 1019150998, 'angied.ruiz', 'Ruiz123', 'Activo', 'Proponente');
+INSERT INTO Usuario (id, tid, nombre, telefono, correo, clave, estado, rol) VALUES (9303130, 'CC', 'Angi Jimenez', 3186759533, 'angi.jimenez', 'Angie2020', 'Activo', 'Administrador');
+INSERT INTO Usuario (id, tid, nombre, telefono, correo, clave, estado, rol) VALUES (1019150998, 'CC', 'Daniela Ruiz', 3178484579, 'angied.ruiz', 'RuizAlf123', 'Activo', 'Proponente');
--- tables.sql
+++ tables.sql
@@ -1,38 +1,12 @@
-CREATE TABLE IF NOT EXISTS Rol(
-tipo VARCHAR(12) PRIMARY KEY);
+CREATE TABLE IF NOT EXISTS Rol(tipo VARCHAR(12) PRIMARY KEY);
 
-CREATE TABLE IF NOT EXISTS Usuario(
-  id NUMBER(12) NOT NULL,
-  tid VARCHAR(2) NOT NULL,
-  nombre VARCHAR(50) NOT NULL,
-  telefono NUMBER(12) NOT NULL,
-  correo VARCHAR(50) NOT NULL,
-  clave VARCHAR(20) NOT NULL,
-  estado VARCHAR(15) NOT NULL,
-  rol VARCHAR(12) REFERENCES Rol(tipo),
-  PRIMARY KEY (correo),
-  UNIQUE KEY (id),
-  UNIQUE KEY (telefono));
+CREATE TABLE IF NOT EXISTS Usuario(id BIGINT NOT NULL, tid VARCHAR(2) NOT NULL, nombre VARCHAR(50) NOT NULL, telefono BIGINT NOT NULL, correo VARCHAR(50) PRIMARY KEY, clave VARCHAR(20) NOT NULL, estado VARCHAR(15) NOT NULL, rol VARCHAR(12) REFERENCES Rol(tipo));
   
-CREATE TABLE IF NOT EXISTS Iniciativa (
-   nombre VARCHAR(50) NOT NULL,
-   id int  NOT NULL,
-   descripcion varchar(150)  NOT NULL,
-   fechaInicio date  NOT NULL,
-   --palabrasClaves varchar(20)  REFERENCES PalabraClave(id), aun no se como ponerlo
-   area varchar(100)  NOT NULL,
-   user_id int REFERENCES Usuario(id),
-   estado VARCHAR(50) NOT NULL,
-   UNIQUE KEY (nombre),
-   PRIMARY KEY (id));
-   
-CREATE TABLE IF NOT EXISTS Estado (
-   id int  NOT NULL,
-   estado varchar(50) NOT NULL,
-   UNIQUE KEY (estado),
-   PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS Iniciativa (nombrePropuesta VARCHAR(50) NOT NULL, id SERIAL PRIMARY KEY, descripcion varchar(150)  NOT NULL, fechaInicio date  NOT NULL, area varchar(100)  NOT NULL, usuario_id INT REFERENCES Usuario(id), estado VARCHAR(50) NOT NULL);
 
-CREATE TABLE IF NOT EXISTS PalabraClave (
-   id int  NOT NULL,
-   palabraClave varchar(20)  NOT NULL,
-   PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS PalabraClave (id INT PRIMARY KEY, palabraClave VARCHAR(20) NOT NULL);
+
+ALTER TABLE Usuario ADD CONSTRAINT id_unico UNIQUE(id);
+
+ALTER TABLE Iniciativa ADD CONSTRAINT nombrePropuesta_unico UNIQUE(nombrePropuesta);
+
