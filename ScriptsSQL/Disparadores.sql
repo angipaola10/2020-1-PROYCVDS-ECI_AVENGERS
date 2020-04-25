415eb252cb26802ed8615fea9d98cf071a686ecc
Edyesid <edyesid2000@gmail.com>
2020-04-24 23:57:58 -0500

    disparador de la base de datos

---
+++ ScriptsSQL/Disparadores.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION funcionFecha() RETURNS trigger AS
+$$
+BEGIN
+	NEW.fechainicio:= current_date;
+	RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER tb_fecha_inicio
+BEFORE INSERT ON public.iniciativa 
+FOR EACH ROW
+EXECUTE PROCEDURE funcionFecha()
