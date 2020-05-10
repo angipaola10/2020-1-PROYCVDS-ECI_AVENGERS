7c2bbb112e120857efeca9c6b63fee9aa84a9bf8
angiedanielar <60035121+angiedanielar@users.noreply.github.com>
2020-05-10 13:38:20 -0500

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -28,4 +28,4 @@ CREATE TABLE IF NOT EXISTS MeInteresa (id SERIAl PRIMARY KEY, idIniciativa INT N
 
 CREATE TABLE IF NOT EXISTS Grupo (idiniciativa int PRIMARY KEY, nombre varchar(30) NOT NULL);
 
-ALTER TABLE Grupo ADD CONSTRAINT fk_grupo_iniciativa FOREIGN KEY (idiniciativa) REFERENCES iniciativa(id);
+ALTER TABLE Grupo ADD CONSTRAINT fk_grupo_iniciativa FOREIGN KEY (idiniciativa) REFERENCES Iniciativa(id);
