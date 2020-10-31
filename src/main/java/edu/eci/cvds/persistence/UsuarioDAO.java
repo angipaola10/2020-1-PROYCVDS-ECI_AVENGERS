760a021bdc8c025d627b56b523f28bb2eadd34e1
angipaola10 <angi9236@gmail.com>
2020-10-30 21:44:43 -0500

    prueba rama

--- src/main/java/edu/eci/cvds/persistence/UsuarioDAO.java
+++ src/main/java/edu/eci/cvds/persistence/UsuarioDAO.java
@@ -8,25 +8,16 @@ import java.math.BigInteger;
 
 public interface UsuarioDAO {
 	
-	
-	
 	public List<Usuario> obtenerUsuarios() throws PersistenceException;
-
 
     public Usuario obtenerUsuario(String correo) throws PersistenceException;
 	
-	
 	public Usuario obtenerUsuarioLog(String correo, String clave) throws PersistenceException;
-	
 	
 	public void actualizarUsuario(String rol, String correo) throws PersistenceException;
 	
-	
 	public void agregarUsuario(int id, String tid, String nombre, BigInteger  telefono, String correo, String clave, Rol rol, String estado) throws PersistenceException;
 
-
 	public void actualizarUsuarioEstado(String estado, String correo) throws PersistenceException;
-
-
 
 }
