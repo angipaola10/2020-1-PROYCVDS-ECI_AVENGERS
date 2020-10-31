d9e1dd5944a97efe94c0830738b3c8ae14d3b067
angipaola10 <angi9236@gmail.com>
2020-10-30 21:51:02 -0500

    prueba rama

--- src/main/java/edu/eci/cvds/persistence/UsuarioDAO.java
+++ src/main/java/edu/eci/cvds/persistence/UsuarioDAO.java
@@ -6,7 +6,6 @@ import java.util.*;
 import java.lang.String;
 import java.math.BigInteger;
 
-
 public interface UsuarioDAO {
 	
 	public List<Usuario> obtenerUsuarios() throws PersistenceException;
