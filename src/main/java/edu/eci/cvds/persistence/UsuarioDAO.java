88a9ffb22f2333034868bcc288333cc22f4266dc
angipaola10 <angi9236@gmail.com>
2020-10-30 21:40:23 -0500

    prueba rama

--- src/main/java/edu/eci/cvds/persistence/UsuarioDAO.java
+++ src/main/java/edu/eci/cvds/persistence/UsuarioDAO.java
@@ -1,27 +1,32 @@
 package edu.eci.cvds.persistence;
 
 import edu.eci.cvds.entities.Rol;
-
 import edu.eci.cvds.entities.Usuario;
-
 import java.util.*;
-
 import java.lang.String;
-
 import java.math.BigInteger;
 
 public interface UsuarioDAO {
 	
+	
+	
 	public List<Usuario> obtenerUsuarios() throws PersistenceException;
+
 
     public Usuario obtenerUsuario(String correo) throws PersistenceException;
 	
+	
 	public Usuario obtenerUsuarioLog(String correo, String clave) throws PersistenceException;
+	
 	
 	public void actualizarUsuario(String rol, String correo) throws PersistenceException;
 	
+	
 	public void agregarUsuario(int id, String tid, String nombre, BigInteger  telefono, String correo, String clave, Rol rol, String estado) throws PersistenceException;
 
+
 	public void actualizarUsuarioEstado(String estado, String correo) throws PersistenceException;
+
+
 
 }
