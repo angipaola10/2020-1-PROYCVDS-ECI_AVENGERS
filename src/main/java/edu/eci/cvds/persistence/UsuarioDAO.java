d50a32c142097469f330b0d14617bb98406cded9
angipaola10 <angi9236@gmail.com>
2020-10-30 20:54:09 -0500

    prueba rama

--- src/main/java/edu/eci/cvds/persistence/UsuarioDAO.java
+++ src/main/java/edu/eci/cvds/persistence/UsuarioDAO.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.cvds.persistence;
 
 import edu.eci.cvds.entities.Rol;
+
 import edu.eci.cvds.entities.Usuario;
+
 import java.util.*;
+
 import java.lang.String;
+
 import java.math.BigInteger;
 
 public interface UsuarioDAO {
